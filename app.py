5b4575d1cd7c557e0937b97a178e30da0560376e
F_Naru <48640767+F-Naru@users.noreply.github.com>
2024-07-31 21:52:36 +0900

    ファイルを1秒ごとにロードする機能を追加
    ファイルを1秒ごとにロードする機能を追加を追加しました

--- app.py
+++ app.py
@@ -13,11 +13,14 @@ def index():
 
 @socketio.on('get_subtitles')
 def handle_get_subtitles():
-    # 例として、字幕データをファイルから読み込む
-    with open('subtitles.json', 'r') as f:
-        subtitle_data = json.load(f)
-    # JSONデータをクライアントに送信
-    emit('subtitle', subtitle_data)
+
+
+    while True:
+        # 1秒ごとに字幕データを送信
+        with open('subtitles.json', 'r') as f:
+            subtitle_data = json.load(f)        
+        socketio.sleep(1)
+        emit('subtitle', subtitle_data)
 
 if __name__ == "__main__":
     socketio.run(app, host="0.0.0.0", port=5000)
